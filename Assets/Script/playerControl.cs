9586c6f17aee9b60446927500143caa4dec88172
cheaptricksss <dao9828@nyu.edu>
2022-09-30 11:30:47 -0400

    added sprint, got rid of bugd

---
+++ Assets/Script/playerControl.cs
@@ -0,0 +1,132 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float horizontalMove;
+    public float movementMultiplier;
+
+    //check if the pc is grounded
+    private bool grounded;
+    public float castDistance = 1f;
+
+    //for jumping
+    public float jumpLimit = 100f;
+    public float gravityScale = 5f;
+    public float gravityFall = 40f;
+    private bool jump = false;
+
+    Animator anim;
+
+
+    //for sprinting
+    private bool isWalking = false;
+    public float sprintMultiplier = 1.2f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        grounded = false;
+        
+
+    }
+
+    // Update is called once per frame
+    //tired to fps
+    void Update()
+    {
+        Debug.Log(Input.GetKey("z"));
+        Debug.DrawRay(GetComponent<Transform>().position, new Vector3(0, castDistance*-1, 0), Color.red, 1f, false);
+        horizontalMove = Input.GetAxis("Horizontal");
+        //jumping
+        if (Input.GetButtonDown("Jump") && grounded) //jump default to space
+        {
+            jump = true;
+        }
+    }
+
+    //physics always calculated on FixedUpdate
+    //no input things in FixedUpdate
+    private void FixedUpdate()
+    {
+        HorizontalMove(horizontalMove);
+
+        if (jump)
+        {
+            rb.AddForce(Vector2.up * jumpLimit, ForceMode2D.Impulse); //impulse apply the impulse force instantly.
+            jump = false;
+        }
+
+        if (rb.velocity.y >= 0)
+        {
+            rb.gravityScale = gravityScale;
+        }
+        else if (rb.velocity.y < 0)
+        {
+            rb.gravityScale = gravityFall;
+        }
+
+
+        
+        //sprinting
+        if ((-1f<= horizontalMove && horizontalMove <=1f && horizontalMove!=0))
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+
+        if(isWalking == true && Input.GetKey("z")) 
+        {
+            sprintMultiplier = 1.8f;
+        }
+        else
+        {
+            sprintMultiplier = 1;
+        }
+        //hello
+        //Physics.RayCast(Origin, direction, maxDistance)
+        //Scripting API: https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
+        //Vector2.down = Vector2(0, -1) (shorthand version)
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, castDistance); //An inbuild physics function.
+                                                                                              //Debug.DrawRay(transform.position, Vector2.down*castDistance, new Color(255,0,0));
+        //Debug.Log(hit.transform.name);
+        //moving player to layer-> Ignored RayCast so the Log does not print player name
+        if (hit.collider != null && hit.collider.tag == "floor")
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+
+
+    }
+
+    void HorizontalMove(float toMove)
+    {
+        float moveX = toMove * Time.fixedDeltaTime * movementMultiplier*sprintMultiplier; //the time that fixed update is running
+        rb.velocity = new Vector3(moveX, rb.velocity.y);
+        if (rb.velocity.x > 0 || rb.velocity.x < 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
+
+//player jumping. On the ground jumping.
+    //check the player is on the ground.
+        //RayCasting: draw a ray, goes down, detect if the player is on the ground.
